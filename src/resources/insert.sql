7003d3d3ba56ae51cae17cc945375e2e3608e8f8
Victor Malyshev <vmalyshev0@gmail.com>
2016-06-17 15:03:30 +0300

    Renaming old insert file

--- src/resources/insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO CUSTOMER_CARDS(card_id, currentBalance, pin, isLocked) VALUES (1, 100, 1111, false);
-INSERT INTO CUSTOMER_TRANSACTIONS(cardTransaction_id, cardTransactionCode, cardTransactionDate, amount, card_id) VALUES (1, '0', '2016-06-17', 0, 1);
-INSERT INTO CUSTOMER_TRANSACTIONS(cardTransaction_id, cardTransactionCode, cardTransactionDate, amount, card_id) VALUES (2, '1', '2016-06-16', 10, 1);
-INSERT INTO CUSTOMER_TRANSACTIONS(cardTransaction_id, cardTransactionCode, cardTransactionDate, amount, card_id) VALUES (3, '1', '2016-06-15', 10, 1);
-INSERT INTO CUSTOMER(customer_id, customerName, card_id) VALUES (1, 'User1', 1);
